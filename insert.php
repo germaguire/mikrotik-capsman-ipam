61162ae57018e8a1b587a24e028a7fbdacd4ccf1
germaguire <32132367+germaguire@users.noreply.github.com>
2021-07-11 13:06:13 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -14,7 +14,7 @@
 <body>
 <div class="container">
 <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-<a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+<a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
 <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
 <span class="fs-4">Mikrotik IPAM</span>
 </a>
